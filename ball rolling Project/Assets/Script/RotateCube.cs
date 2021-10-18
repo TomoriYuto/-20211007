3fbd9ce1b49c3dc4a9ad49767df897bc00e4d4b2
KBC12006108\kbc12006108 <yuto-tomori-20h@stu.kbc.ac.jp>
2021-10-18 12:53:32 +0900

    プロジェクト修正

--- ball rolling Project/Assets/Script/RotateCube.cs
+++ ball rolling Project/Assets/Script/RotateCube.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//Ax = Tx.localEulerAngles.x; //W,S
+//Az = Tz.localEulerAngles.z; //A,D
+
+//Transform Tx, Tz;
+//float Ax, Az;
+//Tx = GetComponent<Transform>();
+//Tz = GetComponent<Transform>();using UnityEngine;
+
 using UnityEngine;
 
 public class RotateCube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject aCube;
+    [SerializeField]
+    private GameObject bCube;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 5));
+        if (Input.GetMouseButtonDown(0))
+        {
+            aCube.transform.localRotation *= Quaternion.Euler(0, 30, 0); //45度回転!
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            var aPos = aCube.transform.localPosition;
+            var bPos = bCube.transform.localPosition;
+            aCube.transform.localPosition = bPos;
+            bCube.transform.localPosition = aPos;
+        }
     }
 }
