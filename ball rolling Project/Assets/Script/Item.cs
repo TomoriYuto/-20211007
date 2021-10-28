7bac616dd75026bb828d9ba591eabbccafb86302
KBC12006108\kbc12006108 <yuto-tomori-20h@stu.kbc.ac.jp>
2021-10-28 14:22:33 +0900

    コミット

---
+++ ball rolling Project/Assets/Script/Item.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider hit)
+    {
+       // 接触対象はPlayerか
+        if (hit.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//public class Item : MonoBehaviour
+//{
+
+//    void OnCollisionEnter(Collision other)
+//    {
+//        if (other.gameObject.tag == "Ball")
+//        {
+//            Destroy(gameObject, 0.2f);
+//        }
+//    }
+//}
