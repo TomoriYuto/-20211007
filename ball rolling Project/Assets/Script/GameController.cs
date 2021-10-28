ca4077c5fcf19c74d65e760e21c75234334d863c
KBC12005095\kbc12005095 <yua-nakaema-20f@stu.kbc.ac.jp>
2021-10-28 14:19:00 +0900

    アイテムの取得カウント

---
+++ ball rolling Project/Assets/Script/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController: MonoBehaviour
+{
+    //public float speed;
+    public Text countText;
+
+    private Rigidbody rb;
+    private int count; // アイテムの取得数を格納する変数
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0; // 初期化
+        SetCountText();
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        //rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1; // 衝突判定のイベントが発生した際に count の数を１上げる
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = count.ToString() + " / 12";
+    }
+}
