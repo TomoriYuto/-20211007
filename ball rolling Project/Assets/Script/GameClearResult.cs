a221c4b8a27cdb8f32dc2f7cde42fe646a5ce483
KBC12005009\kbc12005009 <kodai-matayoshi-20f@stu.kbc.ac.jp>
2021-11-18 15:04:42 +0900

    ゲームクリア後のシーケンス

---
+++ ball rolling Project/Assets/Script/GameClearResult.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameClearResult : MonoBehaviour
+{
+    [SerializeField]
+    private Text _textCountdown;
+
+    [SerializeField] private GameObject Panel2;
+    [SerializeField] private GameObject Result;
+
+    public Text countText;
+    public Text countResult;
+    public Text ResultTime;
+
+    private int count; // アイテムの取得数を格納する変数
+    private bool inGame;
+
+    private Rigidbody rb;
+
+    private int count2;
+    float seconds = 0;
+    int minute = 0, seconds2 = 0;
+    private float oldSeconds;
+
+    private Text textResult;
+    private Text textScore;
+    private Text textResultTime;
+
+    void Start()
+    {
+        Panel2.SetActive(false);
+        Result.SetActive(false);
+
+        inGame = true;
+        rb = GetComponent<Rigidbody>();
+        count = 0; // 初期化
+        SetCountText();
+
+        _textCountdown.text = "";
+        rb.useGravity = true;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1; // 衝突判定のイベントが発生した際に count の数を１上げる
+            //count2 += 1; 
+            SetCountText();
+        }
+    }
+    public void Update()
+    {
+        if (count >= 12)
+        {
+            if (Input.GetKeyDown("joystick button 0"))
+            {
+                Result.SetActive(false);
+                Panel2.SetActive(true);
+            }
+            if (count == 12)
+            {
+                StartCoroutine(CountdownCoroutine());
+                count += 1;
+            }
+        }
+        else
+        {
+            seconds += Time.deltaTime;
+            if (seconds >= 60f)
+            {
+                minute++;
+                seconds = seconds - 60;
+            }
+            if ((int)seconds != (int)oldSeconds)
+            {
+
+                ResultTime.text = "掛かった時間　" + minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+
+            }
+            oldSeconds = seconds;
+
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = count.ToString() + " / 12";
+        countResult.text = string.Format("取得したコイン　{00}枚", count);
+        //ResultTime.text = string.Format("かかった時間　{0}：{00}", minute, second);
+
+        if (count >= 12)
+        {
+            inGame = false;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            GameObject.Find("Cube").GetComponent<PouseMenu>().enabled = false;
+        }
+    }
+    IEnumerator CountdownCoroutine()
+    {
+        GameObject.Find("Cube").GetComponent<RotateCube>().enabled = false;
+        _textCountdown.gameObject.SetActive(true);
+
+        _textCountdown.text = "ゲームクリア";
+        yield return new WaitForSeconds(1.0f);
+
+        Result.SetActive(true);
+
+        _textCountdown.text = "";
+        _textCountdown.gameObject.SetActive(false);
+    }
+}
---
+++ ball rolling Project/Assets/Script/RetryChoice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RetryChoice : MonoBehaviour
+{
+    public Button button;
+    public Button button2;
+    public RectTransform a;
+
+    void OnEnable()
+    {
+        button2.GetComponent<Button>().Select();
+        //button = GameObject.Find("menu/Panel/re").GetComponent<Button>();
+        //ボタンが選択された状態になる
+        button.GetComponent<Button>().Select();
+
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown("joystick button 10"))
+    //    {
+    //        a.position += new Vector3(1f + 1, 0, 0);
+    //    }
+    //}
+}
