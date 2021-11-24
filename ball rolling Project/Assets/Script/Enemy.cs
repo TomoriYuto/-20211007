17c55316587ed466eee653787902802b80ccf4d5
KBC12005095\kbc12005095 <yua-nakaema-20f@stu.kbc.ac.jp>
2021-11-24 13:59:45 +0900

    お邪魔アイテムとエフェクト

---
+++ ball rolling Project/Assets/Script/Enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject muzzlePoint;
+    public GameObject ball;
+    public float speed = 10f;
+    private int attackTime = 0;
+    public int intvalTime = 30;
+    public GameObject target;
+    private bool inArea = false;
+    //public GameObject bullethit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackTime += 1;
+        if(attackTime % intvalTime == 0)
+        {
+            EneCannonShot();
+        }
+    }
+
+    public void EneCannonShot()
+    {
+        if(target.activeInHierarchy == false)
+        {
+            inArea = false;
+        }
+
+        if(inArea == true)
+        {
+            Vector3 mballPos = muzzlePoint.transform.position;
+            GameObject newBall = Instantiate(ball, mballPos, transform.rotation);
+            Vector3 dir = muzzlePoint.transform.forward;
+
+            newBall.GetComponent<Rigidbody>().AddForce(dir * speed, ForceMode.Impulse);
+            newBall.name = ball.name;
+            Destroy(newBall, 1.0f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), Time.deltaTime * 3.0f);
+
+            inArea = true;
+            target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            inArea = false;
+        }
+    }
+}
---
+++ ball rolling Project/Assets/Script/bulleteffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulleteffect : MonoBehaviour
+{
+    public GameObject bullethit;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Instantiate(bullethit, this.transform.position, Quaternion.identity); //パーティクル用ゲームオブジェクト生成
+        }
+    }
+}
